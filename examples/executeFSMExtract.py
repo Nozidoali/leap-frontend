f9777564705ee0d2e55c6c9f1bfc91696cd1dbc4
Carmine50 <crizzi@ethz.ch>
2024-09-27 01:26:03 +0200

    Added temporary version of script

--- examples/executeFSMExtract.py
+++ examples/executeFSMExtract.py
@@ -6,13 +6,23 @@ if __name__ == "__main__":
     from frontend import *
     import pygraphviz as pgv
 
-    network: Netlist = readVerilog("examples/verilogs/external/legup2.v")
+    network: Netlist = readVerilog("examples/verilogs/external/legup.v")
+    #network: Netlist = readVerilog("examples/verilogs/external/vitis.v")
     module = network.getModuleAt(0)
-    graph: pgv.AGraph = exportDOT(module)
+    #graph: pgv.AGraph = exportDOT(module)
+    graph: pgv.AGraph = exportDOT(module, params={"skipConstants":False, "skipSignals":[]})
 
+    ctrl_names = ["ap_done", "ap_idle", "ap_ready", "finish"]
     outputsNames = [
-        port for port in module.getPortsByType(PortDirection.OUTPUT) if port != "finish"
+        port for port in module.getPortsByType(PortDirection.OUTPUT) if port not in ctrl_names
     ]
 
-    extractDataFlowControlFlow(graph, outputsNames)
-    graph.write("tmp.dot")
+    outputDot = "toy"
+    extractDataFlowControlFlow(module, graph, outputsNames)
+    graph.write("{}.dot".format(outputDot))
+
+    newModule = graphToBNGraph(module, graph, "cluster_control_flow")
+    writeVerilog(newModule, "out.v")
+
+    #CDFG = buildOriginalCDFG(graph, module)
+    #CDFG.write("{}_CDFG.dot".format(outputDot))
