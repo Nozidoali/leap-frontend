285140f7c1271213896c629ecfa045583e27b721
Carmine50 <crizzi@ethz.ch>
2024-10-03 19:34:59 +0200

    Added FSM graph extraction and updated extraction of states correlation with datapath nodes

--- frontend/algorithms/extractFSM.py
+++ frontend/algorithms/extractFSM.py
@@ -3,29 +3,102 @@ from .highlight import *
 import pygraphviz as pgv
 
 
-def _extractDataFlowNodesRec(graph: pgv.AGraph, node: pgv.Node, visited: set):
+def _extractDataFlowNodesRec(module:Module, graph: pgv.AGraph, node: pgv.Node, visited: set, revTraversal: bool):
     if node in visited:
         return
     visited.add(node)
-    for edge in graph.in_edges(node):
+    if revTraversal:
+        edges = graph.in_edges(node)
+    else:
+        edges = graph.out_edges(node)
+    for edge in edges:
         # do not traverse condition
-        if graph.get_edge(edge[0], edge[1]).attr["style"] != "dashed":
-            _extractDataFlowNodesRec(graph, edge[0], visited)
+        if isEdgeCond(edge[0], edge[1], graph, module) == False: 
+            if revTraversal:
+                nextNode = edge[0]
+            else:
+                nextNode = edge[1]
+            _extractDataFlowNodesRec(module, graph, nextNode, visited, revTraversal)
+
+def isExprCondition(expr: BNode):
+    if expr.variable_name == "&&" or expr.variable_name == "||" or expr.variable_name == "!":
+        return True
+    if expr.variable_name == "==" or expr.variable_name == "!=":
+        return True
+    if expr.variable_name == "<" or expr.variable_name == "<=" or expr.variable_name == ">" or expr.variable_name == ">=":
+        return True
+    return False
+
+def isEdgeCond(src: pgv.Node, dst: pgv.Node, graph: pgv.AGraph, module: Module):
+    isCtrl = graph.get_edge(src, dst).attr["style"] == "dashed"
+    if isCtrl:
+        return True
+    
+    if module.isDefined(dst.get_name()):
+        for target, expression, cond in module.node2assignment.keys():
+            if expression == src.get_name() and target == dst.get_name():
+                assign = module.node2assignment[(target, expression, cond)]
+                expr = assign.expression
+                if isExprCondition(expr):
+                    return True
+                break
+    return False
 
 
-def extractDataFlowControlFlowNodes(graph: pgv.AGraph, dataOutputs: list):
+def checkDataFlowNode_rec(module: Module, node: pgv.Node, origNode: pgv.Node , graph: pgv.AGraph, additionalNodes: set, existingNodes: set):
+    successTrav = False
+    for src, dst in graph.out_edges(node):
+        if dst in existingNodes or dst in additionalNodes:
+            continue
+        if isEdgeCond(src, dst, graph, module):
+            if src != origNode:
+                additionalNodes.remove(src) # it's added by next function
+                _extractDataFlowNodesRec(module, graph, src, additionalNodes, True)
+            successTrav = True
+            continue
+
+        additionalNodes.add(dst)
+        status = checkDataFlowNode_rec(module, dst, origNode, graph, additionalNodes, existingNodes)
+        if not status:
+            additionalNodes.remove(dst)
+        else:
+            successTrav = True
+            if src != origNode:
+                additionalNodes.remove(src) # it's added by next function
+                _extractDataFlowNodesRec(module, graph, src, additionalNodes, True)
+
+    if not successTrav:
+        return False
+
+    return True
+
+def extractDataFlowControlFlowNodes(module: Module, graph: pgv.AGraph, dataOutputs: list, dataInputs: list):
     dataNodes = set()
     controlNodes = []
     for out in dataOutputs:
-        _extractDataFlowNodesRec(graph, out, dataNodes)
+        _extractDataFlowNodesRec(module, graph, out, dataNodes, True)
+    origNodes = dataNodes.copy()
+    dataNodesNew = set()
+    for inp in dataInputs:
+        _extractDataFlowNodesRec(module, graph, inp, dataNodesNew, False)
+    dataNodes.update(dataNodesNew)
+    allAdditionalNodes = set()
+    ## make sure that the condition is correctly captured
+    for node in dataNodes:
+        additionalNodes = set()
+        additionalNodes.add(node)
+        checkDataFlowNode_rec(module, node, node, graph, additionalNodes, dataNodes)
+        if len(additionalNodes) > 1:
+            allAdditionalNodes.update(additionalNodes)
+    dataNodes.update(allAdditionalNodes)
     for node in graph.nodes():
         if node not in dataNodes:
             controlNodes.append(node)
     return dataNodes, controlNodes
 
 
-def extractDataFlowControlFlow(module: Module, graph: pgv.AGraph, dataOutputs: list):
-    dataNodes, controlNodes = extractDataFlowControlFlowNodes(graph, dataOutputs)
+def extractDataFlowControlFlow(module: Module, graph: pgv.AGraph, dataOutputs: list, dataInputs: list):
+    dataNodes, controlNodes = extractDataFlowControlFlowNodes(module, graph, dataOutputs, dataInputs)
     dataGraph = graph.add_subgraph(
         dataNodes,
         name="cluster_data_flow",
@@ -93,6 +166,84 @@ def extractDataFlowControlFlow(module: Module, graph: pgv.AGraph, dataOutputs: l
 
     graph.layout(prog="dot")
 
+def findCondVar(node: BNode, op: str , var: str):
+    if node.variable_name == op:
+        if node.children[0].variable_name == var:
+            return node
+    for child in node.children:
+        res = findCondVar(child, op, var)
+        if res is not None:
+            return res
+    return None
+
+def extractFSMGraph(module: Module, graph: pgv.AGraph):
+    CFG = graph.get_subgraph("cluster_control_flow")
+    FSM = pgv.AGraph(strict=False, directed=True)
+    FSM.graph_attr["splines"] = "ortho"
+    FSM.graph_attr["rankdir"] = "TB"  # Top-to-bottom layout
+
+    states = module.getPortsByType(PortType.LOCALPARAM)
+    states += module.getPortsByType(PortType.PARAMETER)
+
+    statesVar = []
+    for assign in module.assignments:
+        if assign.expression.toString() in states:
+            if assign.target.toString() not in statesVar:
+                statesVar.append(assign.target.toString())
+    if len(statesVar) == 0:
+        raise Exception("No next state variable found")
+    if len(statesVar) > 2:
+        raise Exception("More than two states variable found")
+
+    currStateVar = None
+    nextStateVar = None
+    for var in statesVar:
+        statesTmp = states.copy()
+        for assign in module.getAssignmentsOf(var):
+            if assign.expression.toString() in statesTmp:
+                statesTmp.remove(assign.expression.toString())
+        if len(statesTmp) == 0:
+            if nextStateVar is not None:
+                raise Exception("More than one next state variable found")
+            nextStateVar = var
+        else:
+            if currStateVar is not None:
+                raise Exception("More than one current state variable found")
+            currStateVar = var
+
+    foundCurrStateAssign = False
+    for assign in module.getAssignmentsOf(currStateVar):
+        if assign.expression.toString() == nextStateVar:
+            foundCurrStateAssign = True
+            break
+    assert foundCurrStateAssign, "The current and next state variables are not connected"
+
+    startState = None
+    for assign in module.getAssignmentsOf(currStateVar):
+        if assign.expression.toString() in states:
+            assert startState is None, "More than one reset state found"
+            startState = assign.expression.toString()
+    
+    if startState is None:
+        raise Exception("No reset state found")
+    
+    for state in states:
+        FSM.add_node(state, shape="ellipse", color="green")
+
+    for assign in module.getAssignmentsOf(nextStateVar):
+        if assign.condition is None:
+            continue
+        stateDst = assign.expression.toString()
+        if stateDst == currStateVar:
+            continue
+        condition = assign.condition
+        condCurrState = findCondVar(condition, "==", currStateVar)
+        condCurrStateExpr = condCurrState.children[1].variable_name
+        assert condCurrStateExpr in states, "The condition is not a state variable"
+        stateSrc = condCurrStateExpr
+        FSM.add_edge(stateSrc, stateDst, color="red")
+
+    return FSM
 
 def isOpNode(node: pgv.Node):
     return node.attr["shape"] == "ellipse"
@@ -102,100 +253,101 @@ def isVarNode(node: pgv.Node):
     return node.attr["shape"] == "box"
 
 
-def getCondAssignment(module: Module, node: pgv.Node, cond: str):
-    print(node)
-    for assignment in module.getAssignmentsOf(node.get_name()):
-        print(cond, assignment.condition.toString())
-        if assignment.condition is not None and assignment.condition == cond:
-            return assignment.expression
-    return None
-
-
-def isStateNode(module: Module, node: pgv.Node):
-    variable_type = module.getVariableType(node.get_name())
-    return variable_type == PortType.LOCALPARAM
-
-
 def getArrivalState_rec(
-    CDFG: pgv.AGraph,
+    CFG: pgv.AGraph,
     node: pgv.Node,
     visited: set,
-    reachedStates: set,
     module: Module,
-    end_node: str,
+    end_nodes: list,
+    FSM: pgv.AGraph,
 ):
     if node in visited:
         return None
-    print(node)
     visited.add(node)
-    if node.attr["label"] == end_node:
-        reachedStates.add(node)
-        return None
-    for src, dst in CDFG.out_edges(node):
-        print(src, dst.attr["label"])
+    if node in FSM.nodes():
+        return node
+    if node.attr["label"] in end_nodes:
+        return node
+    for src, dst in CFG.out_edges(node):
         if isOpNode(dst):
             op_value = dst.attr["label"]
             if op_value == "~":
                 continue
-        elif isVarNode(dst):
-            var_name = dst.attr["label"]
-            isCond = "cond" in CDFG.get_edge(src, dst).attr["xlabel"]
-            if var_name == end_node:
-                reachedStates.add(dst)
-                return None
-            if isCond:
-                assignedValue = getCondAssignment(module, dst, var_name)
-                print(var_name)
-                assert assignedValue is not None
-                if isStateNode(assignedValue):
-                    reachedStates.add(dst)
-                    continue
         dstNode = getArrivalState_rec(
-            CDFG, dst, visited, reachedStates, module, end_node
+            CFG, dst, visited, module, end_nodes, FSM
         )
+        if dstNode is not None:
+            return dstNode
     return None
 
 
 # function to extract the arrival states of the data-controlled CFG
 def getArrivalStates(
-    graph: pgv.AGraph, controlPaths: list, module: Module, end_node: str
+    graph: pgv.AGraph, controlPaths: list, module: Module, end_nodes: list, FSM: pgv.AGraph
 ):
     arrivalStates = []
     CFG = graph.get_subgraph("cluster_control_flow")
     for dataNode, ctrlNode in controlPaths:
-        reachedStates = set()
+        controlInputNode = graph.out_edges(ctrlNode)[0][1]
         dstState = getArrivalState_rec(
-            graph, ctrlNode, set(), reachedStates, module, end_node
+            CFG, controlInputNode, set(), module, end_nodes, FSM
         )
         assert dstState is not None
         arrivalStates.append((dataNode, dstState))
     return arrivalStates
 
+# function to extract the departure states of the ctrl data CFG
+def getDepartureStates(graph: pgv.AGraph, controlPaths: list, module: Module, FSM: pgv.AGraph):
+    departureStates = []
+    for ctrlNode, dataNode in controlPaths:
+        assert len(graph.in_edges(ctrlNode)) == 1
+        controlOutputNode = graph.in_edges(ctrlNode)[0][0]
+        print(controlOutputNode.get_name(), module.getDependencies(controlOutputNode.get_name()))
+    return departureStates
+
 
 # function to build the original CDFG with the extracted data flow
-def buildOriginalCDFG(graph: pgv.AGraph, module: Module, end_node: str = "finish"):
+def buildOriginalCDFG(graph: pgv.AGraph, module: Module, FSM: pgv.AGraph, end_nodes: list):
     CDFG = pgv.AGraph(strict=False, directed=True)
     CDFG.graph_attr["splines"] = "ortho"
     CDFG.graph_attr["rankdir"] = "TB"  # Top-to-bottom layout
 
-    for srcDataNode, dstDataNode in graph.get_subgraph("cluster_data_flow").edges():
-        CDFG.add_edge(srcDataNode, dstDataNode, color="blue", style="dashed")
+    inCtrlOutDataWire = []
+    outCtrlInDataWire = []
 
     # collect all the control paths that come from data flow nodes
-    controlPaths = []
     for srcNode, dstNode in graph.edges():
         srcIsDataNode = srcNode in graph.get_subgraph("cluster_data_flow").nodes()
         dstIsDataNode = dstNode in graph.get_subgraph("cluster_data_flow").nodes()
         if srcIsDataNode and dstIsDataNode:
-            CDFG.add_edge(srcNode, dstNode, color="blue", style="dashed")
+            CDFG.add_edge(srcNode.get_name(), dstNode.get_name(), color="blue", style="dashed")
         elif srcIsDataNode:
             ## if the source node is a data node and the destination node is a control node
             ## this is a control path from data to control
-            CDFG.add_node(srcNode)
-            controlPaths.append((srcNode, dstNode))
+            CDFG.add_node(srcNode.get_name())
+            inCtrlOutDataWire.append((srcNode, dstNode))
         elif dstIsDataNode:
-            CDFG.add_node(dstNode)
+            CDFG.add_node(dstNode.get_name())
+            outCtrlInDataWire.append((srcNode, dstNode))
 
-    arrivalStates = getArrivalStates(graph, controlPaths, module, end_node)
+    print(inCtrlOutDataWire, "AAAAAA")
+
+    for node in CDFG.nodes():
+        name = graph.get_node(node).attr["label"]
+        CDFG.get_node(node).attr["label"] = name
+        shape = graph.get_node(node).attr["shape"]
+        CDFG.get_node(node).attr["shape"] = shape
+
+    arrivalStates = getArrivalStates(graph, inCtrlOutDataWire, module, end_nodes, FSM)
+    departureStates = {}
+    #departureStates = getDepartureStates(graph, outCtrlInDataWire, module, FSM)
+    for dataNode, arrivalState in arrivalStates:
+        if arrivalState in end_nodes:
+            CDFG.add_edge(dataNode.get_name(), arrivalState.get_name(), color="red", style="dashed")
+        elif arrivalState in departureStates.keys():
+            pass
+            #CDFG.add_edge(dataNode, arrivalState, color="green", style="dashed")
+        else:
+            assert False, "Arrival state not found"
 
     return CDFG
