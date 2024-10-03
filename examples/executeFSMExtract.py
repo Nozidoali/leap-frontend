989931a669c046e6174e67a8b332235ea590e63e
Carmine50 <crizzi@ethz.ch>
2024-10-03 19:36:17 +0200

    Updated script to reverse engineering verilog

--- examples/executeFSMExtract.py
+++ examples/executeFSMExtract.py
@@ -14,19 +14,29 @@ if __name__ == "__main__":
         module, params={"skipConstants": False, "skipSignals": []}
     )
 
-    ctrl_names = ["ap_done", "ap_idle", "ap_ready", "finish"]
+    ctrl_output_names = ["ap_done", "ap_idle", "ap_ready", "finish", "ap_start"]
+    ctrl_input_names = ["start", "memory_controller_waitrequest", "clk", "clk2x", "clk1x_follower", "reset", "rst", "ap_rst", "ap_reset"]
     outputsNames = [
         port
         for port in module.getPortsByDir(PortDirection.OUTPUT)
-        if port not in ctrl_names
+        if port not in ctrl_output_names
+    ]
+
+    inputsNames = [
+        port
+        for port in module.getPortsByDir(PortDirection.INPUT)
+        if port not in ctrl_input_names
     ]
 
     outputDot = "toy"
-    extractDataFlowControlFlow(module, graph, outputsNames)
+    extractDataFlowControlFlow(module, graph, outputsNames, inputsNames)
     graph.write("{}.dot".format(outputDot))
 
-    newModule = graphToBNGraph(module, graph, "cluster_control_flow")
-    writeVerilog(newModule, "out.v")
+    #newModule = graphToBNGraph(module, graph, "cluster_control_flow")
+    #writeVerilog(newModule, "out.v")
 
-    # CDFG = buildOriginalCDFG(graph, module)
-    # CDFG.write("{}_CDFG.dot".format(outputDot))
+    FSM = extractFSMGraph(module, graph)
+    FSM.write("{}_FSM.dot".format(outputDot))
+
+    CDFG = buildOriginalCDFG(graph, module, FSM, ["finish", "ap_done"])
+    CDFG.write("{}_CDFG.dot".format(outputDot))
